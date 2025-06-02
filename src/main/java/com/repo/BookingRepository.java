f3b7273c3e503f484c31a40ee2db2eb1587ea1d2
blendinamuhadri <bm70318@ubt-uni.net>
2025-06-02 15:50:21 +0200

    sherbimet e booking

--- src/main/java/com/repo/BookingRepository.java
+++ src/main/java/com/repo/BookingRepository.java
@@ -1,16 +1,18 @@
 package com.repo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.entity.Booking;
 
+
 public interface BookingRepository extends JpaRepository<Booking, Long>{
 
     List<Booking> findByRoomId(Long roomId);
 
-    List<Booking> findByBookingConfirmationCode(String confirmationCode);
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
     
     List<Booking> findByUserId(Long userId);
 
