eabd72eaebb10eb86df6e24c70c79dc2be3a8ccb
blendinamuhadri <bm70318@ubt-uni.net>
2025-06-02 14:08:51 +0200

    IBookingService

---
+++ src/main/java/com/service/interfac/IBookingService.java
@@ -0,0 +1,16 @@
+package com.service.interfac;
+
+import com.dto.Response;
+import com.entity.Booking;
+
+public interface IBookingService {
+
+    Response saveBooking(Long roomId, Long userId, Booking bookingRequest);
+
+    Response findBookingByConfirmationCode(String confirmationCode);
+
+    Response getAllBookings();
+
+    Response cancelBooking(Long bookingId);
+
+}
