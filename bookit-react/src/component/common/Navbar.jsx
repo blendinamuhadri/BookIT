69e638f13686256b16040d633866408376fb535c
aldamuhadri <am70315@ubt-uni.net>
2025-06-03 21:44:09 +0200

    Navbar me autentikim, role dhe logout

---
+++ bookit-react/src/component/common/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import ApiService from "../../service/ApiService";
+
+function Navbar() {
+  const isAuthenticated = ApiService.isAuthenticated();
+  const isAdmin = ApiService.isAdmin();
+  const isUser = ApiService.isUser();
+  const navigate = useNavigate();
+
+  const handleLogout  =() =>{
+    const isLogout = window.confirm("Are you sure you really want to logout");
+    if(isLogout){
+        ApiService.logout();
+        navigate('/home')
+    }
+  }
+
+
+    return (
+        <nav className="navbar">
+
+            <div className="navbar-brand">
+                <NavLink to="/home">Phegon Hotel</NavLink>
+            </div>
+
+            <ul className="navbar-ul">
+                <li><NavLink to="/home" activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/rooms" activeClassName="active">Rooms</NavLink></li>
+                <li><NavLink to="/find-booking" activeClassName="active">Find My Bookings</NavLink></li>
+
+                {isUser && (<li><NavLink to="/profile" activeClassName="active">Profile</NavLink></li>)} 
+                {isAdmin && (<li><NavLink to="/admin" activeClassName="active">Admin</NavLink></li>)}
+
+                {!isAuthenticated && <li><NavLink to="/login" activeClassName="active">Login</NavLink></li>}
+                {!isAuthenticated && <li><NavLink to="/register" activeClassName="active">Register</NavLink></li>}
+
+                {isAuthenticated && <li onClick={handleLogout}>Logout</li>}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
